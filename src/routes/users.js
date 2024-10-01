6644560bdba4c83b5945a23245fec7054dbff0ae
pam77777778441 <pamelarib2016@hotmail.com>
2024-09-30 21:37:12 -0300

    aula12

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,41 +1,42 @@
-import express, { request, response } from 'express'
+import express from 'express'
 import { v4 as uuidv4 } from 'uuid'
-import rgrg from 'bcrypt'
+import bcrypt from 'bcrypt'
 
-import {validateUserRegistration} from './../middlewares/validation'
+import { validateUserRegistration } from './../middlewares/validation'
 
 const router = express.Router()
 
+// Exportação nomeada
 export const users = []
 
-router.post('/signup', validateUserRegistration , async (request, response) => {
-    const {name, email, password} = request.body
+router.post('/signup', validateUserRegistration, async (request, response) => {
+  const { name, email, password } = request.body
 
-    const emailAlreadyRegistration = users.find(user => user.email === email)
+  const emailAlreadyRegistration = users.find(user => user.email === email)
 
-    if (emailAlreadyRegistration) {
-        return response.status(404).json({
-            message: 'Email já cadastrado!'
-        })
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10)
-
-    const newUser = {
-        id: uuidv4(),
-        name,
-        email,
-        password: hashedPassword //salva a senha encriptada o array
-    }
-
-    users.push(newUser)
-
-    response.status(201).json({
-        message: 'Conta criada com sucesso'
-        user: newUser 
+  if (emailAlreadyRegistration) {
+    return response.status(400).json({
+      message: 'E-mail já cadastrado.'
     })
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const newUser = {
+    id: uuidv4(),
+    name,
+    email,
+    password: hashedPassword // salva a senha "encriptada" no array
+  }
+
+  users.push(newUser)
+
+  response.status(201).json({
+    message: 'Conta criada com sucesso.',
+    user: newUser
+  })
 })
 
+// Exportação default
 export default router
 
-// localhost:3000/users/signup
